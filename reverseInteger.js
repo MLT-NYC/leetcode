17e0817bc5c98076493491878d11e3e078ded4e0
MLT-NYC <maxtrukhin@gmail.com>
2019-06-02 13:16:29 -0400

    Actually solved reverseInteger using brute-force iteration

--- reverseInteger.js
+++ reverseInteger.js
@@ -3,11 +3,23 @@
 // Brute-force
 var reverse = function (x) {
     const str = x.toString();
-    let revStr = '';
+
+    let revStr;
+    if (x > 0) {
+        revStr = '';
+    } else {
+        revStr = '-';
+    }
 
     for(let i = str.length -1;i >=0;i--) {
         revStr += str[i];
     }
 
-    return parseInt(revStr, 10);
+    let result = parseInt(revStr, 10);
+    if (result < Math.pow(-2, 31) || result > Math.pow(2, 31) - 1) {
+        return 0;
+    } else {
+        return result;
+    }
+
 };
