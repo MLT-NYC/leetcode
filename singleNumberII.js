25f3269d6438d6b2696f37fa2671c20f5f6b4181
MLT-NYC <maxtrukhin@gmail.com>
2020-04-03 21:23:41 -0400

    Solved single number II

---
+++ singleNumberII.js
@@ -0,0 +1,20 @@
+// Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+//
+//     Note:
+//
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+//
+//     Example 1:
+//
+// Input: [2,2,3,2]
+// Output: 3
+// Example 2:
+//
+// Input: [0,1,0,1,0,1,99]
+// Output: 99
+
+var singleNumber = function(nums) {
+    const numsCounter = {};
+    nums.forEach(num => numsCounter[num] ? numsCounter[num] += 1 : numsCounter[num] = 1);
+    for ([key, value] of Object.entries(numsCounter)) if (value < 2) return key;
+};
