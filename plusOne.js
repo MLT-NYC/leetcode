04fb8b0c5a60a8ed1698f4b93e05229ae8d36acc
MLT-NYC <maxtrukhin@gmail.com>
2020-11-28 14:06:44 -0500

    added notes to plusoNE

--- plusOne.js
+++ plusOne.js
@@ -16,40 +16,51 @@
 // Explanation: The array represents the integer 4321.
 
 
-var plusOne = function(digits) {
-    const lastDigit = digits[digits.length-1]
+const plusOne = function(digits) {
+    const lastDigit = digits[digits.length-1];
     const allExceptLast = digits.slice(0, digits.length-1);
     
+
+    // BASE CASE(s)
+    // if there there is only one digit, 
+    // we check to see if incrementing it will result in a single digit or '10',
+    // and we return that result.
     if (digits.length < 2) {
         if (lastDigit < 9) {
-            return [lastDigit + 1]
+            return [lastDigit + 1];
         } else {
             return [1, 0];
         }
     }
-    
+    // if there are multiple digits, and the increment of the last digit is less than '10',
+    // we return the result.    
     if (lastDigit < 9) return [...allExceptLast, lastDigit + 1];
     
-    return [...plusOne(digits.slice(0, digits.length-1)), 0]
+    // RECURSIVE CASE
+    // Otherwise, if there are multiple digits, and incrementing the last will result in '10',
+    // we return the an array with the unpacked contents of a recursive call to plusOne, 
+    // and '0' appended at the end.
+    return [...plusOne(digits.slice(0, digits.length-1)), 0];
 };
 
-var plusOne = function (digits) {
-    const newDigits = [...digits];
 
-    for (let i = newDigits.length - 1; i >= 0; i--) {
-        const currDigit = digits[i];
-        const newDigit = currDigit + 1;
+// var plusOne = function (digits) {
+//     const newDigits = [...digits];
 
-        if (i < 1 && newDigit > 9) {
-            newDigits[i] = 0;
-            newDigits.unshift(1)
-        } else if (newDigit > 9) {
-            newDigits[i] = 0;
-        } else {
-            newDigits[i] = newDigit;
-            break;
-        }
-    }
+//     for (let i = newDigits.length - 1; i >= 0; i--) {
+//         const currDigit = digits[i];
+//         const newDigit = currDigit + 1;
 
-    return newDigits;
-};
+//         if (i < 1 && newDigit > 9) {
+//             newDigits[i] = 0;
+//             newDigits.unshift(1)
+//         } else if (newDigit > 9) {
+//             newDigits[i] = 0;
+//         } else {
+//             newDigits[i] = newDigit;
+//             break;
+//         }
+//     }
+
+//     return newDigits;
+// };
